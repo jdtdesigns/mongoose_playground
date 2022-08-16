8d450e264cde317b45f856a00f81091e4003a681
JD Tadlock <jt7903@gmail.com>
2022-08-16 12:23:12 -0400

    standard subdocs complete

--- models/Group.js
+++ models/Group.js
@@ -1,21 +1,40 @@
 const { Schema, model } = require('mongoose');
 
-const studentSchema = new Schema({
-  name: {
-    type: String,
-    required: true
-  }
-})
+// const studentSchema = new Schema({
+//   name: {
+//     type: String,
+//     required: true
+//   }
+// })
 
 const groupSchema = new Schema({
   group_name: {
     type: String,
     required: true
   },
-  students: [studentSchema]
+  students: [{
+    name: {
+      type: String,
+      required: true
+    }
+  }]
+}, {
+  toJSON: {
+    virtuals: true // include any virtual properties on a client side request
+  }
 });
 
+groupSchema.virtual('student_count').get(function () {
+  return this.students.length;
+});
+
+// groupSchema.methods.validatePassword = function () {
+//   console.log(`Hi, ${this.group_name}`)
+// }
+
+
 const Group = model('Group', groupSchema);
+
 
 // User.deleteMany({}).then(() => console.log('users deleted'));
 
--- routes/api_routes.js
+++ routes/api_routes.js
@@ -2,31 +2,52 @@ const api_router = require('express').Router();
 
 const Group = require('../models/Group');
 
+// Get all groups
 api_router.get('/groups', async (req, res) => {
   const groups = await Group.find();
 
   res.send(groups);
 });
 
+// Create a group
 api_router.post('/groups', async (req, res) => {
   const group = await Group.create(req.body);
 
   res.send(group);
 });
 
+// Create a student
 api_router.post('/students', async (req, res) => {
   const { group_id, name } = req.body;
 
   const group = await Group.findOne({ _id: group_id });
 
-  group.students.push({ name });
+  group.students.push({ name: name });
   group.save();
 
   res.send(group);
-})
+});
 
-api_router.delete('/students', async (req, res) => {
+// Get a student by student ID
+api_router.get('/student', async (req, res) => {
+  const student_id = req.query.student_id;
+  const group_id = req.query.group_id;
+  const group = await Group.findOne({
+    _id: group_id
+  });
 
+  res.send(group.students.id(student_id));
+});
+
+// Delete a student from a group
+api_router.delete('/student', async (req, res) => {
+  const group = await Group.findOne({
+    _id: req.query.group_id
+  });
+  group.students.id(req.query.student_id).remove();
+  group.save();
+
+  res.send(group);
 });
 
 module.exports = api_router;
